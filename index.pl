522d24733b0678bea68b28a3a9140dda8b039dbe
zerzal <40403983+zerzal@users.noreply.github.com>
2019-05-31 14:56:22 -0400

    Update index.pl

--- index.pl
+++ index.pl
@@ -130,9 +130,9 @@ print "In case a technician needs access to the phone, please provide a suggeste
 print "\&nbsp\;\&nbsp\;\&nbsp\;\&nbsp\;ASAP<br><br>";
 print "General description of the problem (required)\:<br><br>";
 
-# print "$FORM{'type'}\n";
+print "$FORM{'type'}<br><br>\n";
 
-if ($FORM{'type'}=Fire) {
+if ($FORM{'type'}="Fire") {
 
     print "FIRE ALARM DIALER LINE(S) NOT SHOWING DIAL TONE AT THE DIALER.\n";
 }
